1a18518423317c69ce2d07fe1b59f5a8f842bae9
Dhia Eddine Jmour <125797700+Jmour-dh@users.noreply.github.com>
2024-04-17 09:43:32 +0200

    livereload

--- src/bubble_time/settings.py
+++ src/bubble_time/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
     'products',
 ]
@@ -36,6 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'bubble_time.urls'
