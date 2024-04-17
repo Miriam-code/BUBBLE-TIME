4386898b4bdcff5c4c979df3d8878db9f1707d00
Dhia Eddine Jmour <125797700+Jmour-dh@users.noreply.github.com>
2024-04-17 14:58:53 +0200

    front register & login

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from users.views import sign_up, sign_in,get_me
 
+app_name = "users"
 urlpatterns = [
   path('register', sign_up, name='register'),
   path('login', sign_in, name='login'),
